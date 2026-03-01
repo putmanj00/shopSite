4d36c9a70170bdb034e7c679e2e02864c281b4c6
putmanj00 <putmanj00@hotmail.com>
2026-03-01 00:48:26 -0500

    feat(22-03): wire Sentry.captureException into both error boundaries
    - app/error.tsx: add @sentry/nextjs import, replace console.error with Sentry.captureException(error)
    - app/global-error.tsx: add @sentry/nextjs import, replace console.error + commented Sentry call with Sentry.captureException(error)

--- app/global-error.tsx
+++ app/global-error.tsx
@@ -2,6 +2,7 @@
 
 import { useEffect } from 'react';
 import Link from 'next/link';
+import * as Sentry from '@sentry/nextjs';
 import { Geist, Geist_Mono } from "next/font/google";
 
 const geistSans = Geist({
@@ -22,9 +23,7 @@ export default function GlobalError({
     reset: () => void;
 }) {
     useEffect(() => {
-        // Log the error to an error reporting service like Sentry
-        console.error('Global Error:', error);
-        // Sentry.captureException(error);
+        Sentry.captureException(error);
     }, [error]);
 
     return (
